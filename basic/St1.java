ba644f79e676fdc498d779647e8237581df53b34
pragadeesh <pragadeesh.s96@gmail.com>
2025-06-22 14:42:20 +0530

    Remove St1, bas, demoif Java files and their compiled classes

--- basic/St1.java
+++ basic/St1.java
@@ -1,3 +1,4 @@
+package basic;
 class St1 {
     public static void main(String[] args){
         System.out.print("Hi Madhan, Wellcome to Java");
---
+++ basic/demoif.java
@@ -0,0 +1,39 @@
+package basic;
+import java.util.Scanner;
+
+// if else... 
+
+// class demoif{
+//     public static void main(String[] args){
+//         Scanner scan = new Scanner(System.in);
+//         boolean alive = Boolean.parseBoolean(scan.nextLine()); // true in java "T" in small
+
+//         if(alive){
+//             System.out.print("Vealangidum");
+//         }
+//         else{
+//             System.out.print("Romba nallathu");
+//         }
+//     }
+// }
+
+// getting input from user and asign in variable then we move that into if - else ....
+
+class demoif{
+    public static void main(String agrs[]){
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        scan.nextLine();
+        int num2 = scan.nextInt();
+        
+        if(num1==num2){
+            System.out.print("Yep both nums are Equal");
+        }
+        // elif(num1>num2){
+        //     System.out.print("Boom number one is greater then number two..."+ num1);
+        // }
+        else{
+            System.out.print("Ohho no... nums are not equal and num one is not greater then num1");
+        }
+    }
+}
