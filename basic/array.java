e4adb842da580d411b073affaba31e4cb246d1d6
pragadeesh <pragadeesh.s96@gmail.com>
2025-06-22 22:09:15 +0530

    Add array.java to implement basic array input and output functionality

---
+++ basic/array.java
@@ -0,0 +1,19 @@
+// basic array that hoe works
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class array{
+    public static void main(String[] agrs){
+        Scanner scan = new Scanner(System.in);
+        int[] nums = new int[5];
+        
+        nums[0] = scan.nextInt();
+        nums[1] = scan.nextInt();
+        nums[2] = scan.nextInt();
+        nums[3] = scan.nextInt();
+        nums[4] = scan.nextInt();
+
+        System.out.print(Arrays.toString(nums));
+    }
+}
