c60ed98dc815d6862678b6379a59005bb4390cd2
pragadeesh <pragadeesh.s96@gmail.com>
2025-06-22 13:18:20 +0530

    Add bas.java to handle student information input and output

---
+++ bas.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class bas{
+    public static void main(String[] args){
+        // name, mark, department
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        double mark = scan.nextDouble(); // in java float is consider as a Douvle
+        scan.nextLine();
+        String dep = scan.nextLine();
+
+        System.out.println("Student name is "+ name); // Student name is madhan
+        System.out.println("Student's mark is "+ mark/10 +"/10"); // Student's mark is 8.7/10
+        System.out.print("Student's department is "+ dep); // Student's department is BBA
+
+    }
+}
