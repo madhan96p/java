0c4ab3f54b8de03fb0cd0b3029060bafd5afe315
pragadeesh <pragadeesh.s96@gmail.com>
2025-06-22 13:10:08 +0530

    first commit

---
+++ St1.java
@@ -0,0 +1,5 @@
+class St1 {
+    public static void main(String[] args){
+        System.out.print("Hi Madhan, Wellcome to Java");
+    }
+}
---
+++ nw.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class nw{
+    public static void main(String[] args){
+        // name, mark, department
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        int mark = scan.nextInt();
+        scan.nextLine();
+        String dep = scan.nextLine();
+
+        System.out.println("Student name is "+ name);
+        System.out.println("Students mark is "+ mark);
+        System.out.print("Students department is "+ )
+
+    }
+}
